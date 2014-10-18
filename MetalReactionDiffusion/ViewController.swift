756f1339a61d7a6dcfd65961a7339d34413a2a86
Simon Gladman <sgladman@gmail.com>
2014-10-18 17:20:18 +0100

    Almost there - I think :)

--- MetalReactionDiffusion/ViewController.swift
+++ MetalReactionDiffusion/ViewController.swift
@@ -16,10 +16,15 @@ import QuartzCore
 
 class ViewController: UIViewController
 {
+    private let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+    private let bitmapInfo:CGBitmapInfo = CGBitmapInfo(CGImageAlphaInfo.NoneSkipFirst.toRaw())
+    
     var defaultLibrary: MTLLibrary! = nil
     var device: MTLDevice! = nil
     var commandQueue: MTLCommandQueue! = nil
 
+    let imageView =  UIImageView(frame: CGRectMake(0, 0, 200, 200))
+    
     var texture: MTLTexture!
     
     var outTexture: MTLTexture!
@@ -28,13 +33,10 @@ class ViewController: UIViewController
     {
         super.viewDidLoad()
         
-        setUpMetal()
-
-        //-----
-   
         
-        // let imageView =  UIImageView(frame: CGRectMake(0, 0, 200, 200))
-     
+        view.addSubview(imageView)
+        
+        setUpMetal()
     }
 
     func setUpTexture()
@@ -57,15 +59,14 @@ class ViewController: UIViewController
         
         CGContextDrawImage(context, CGRectMake(0, 0, CGFloat(imageWidth), CGFloat(imageHeight)), imageRef)
         
-        let textureDescriptor = MTLTextureDescriptor.texture2DDescriptorWithPixelFormat(MTLPixelFormat.RGBA16Unorm, width: Int(imageWidth), height: Int(imageHeight), mipmapped: true)
+        let textureDescriptor = MTLTextureDescriptor.texture2DDescriptorWithPixelFormat(MTLPixelFormat.RGBA8Unorm, width: Int(imageWidth), height: Int(imageHeight), mipmapped: true)
         
         texture = device.newTextureWithDescriptor(textureDescriptor)
         
-        let outTextureDescriptor = MTLTextureDescriptor.texture2DDescriptorWithPixelFormat(texture.pixelFormat, width: Int(imageWidth), height: Int(imageHeight), mipmapped: false)
+        let outTextureDescriptor = MTLTextureDescriptor.texture2DDescriptorWithPixelFormat(texture.pixelFormat, width: texture.width, height: texture.height, mipmapped: false)
         outTexture = device.newTextureWithDescriptor(outTextureDescriptor)
         
         let region = MTLRegionMake2D(0, 0, Int(imageWidth), Int(imageHeight))
-  
         texture.replaceRegion(region, mipmapLevel: 0, withBytes: &rawData, bytesPerRow: Int(bytesPerRow))
     }
  
@@ -90,9 +91,6 @@ class ViewController: UIViewController
         commandEncoder.setComputePipelineState(value)
         commandEncoder.setTexture(texture, atIndex: 0)
         commandEncoder.setTexture(outTexture, atIndex: 1)
-        
-        // let buffer = device.newBufferWithLength(outTexture.arrayLength, options: MTLResourceOptions.OptionCPUCacheModeDefault)
-        // commandEncoder.setBuffer(buffer, offset: 0, atIndex: 1)
 
         let threadGroupCount = MTLSizeMake(8, 8, 1)
         let threadGroups = MTLSizeMake(texture.width / threadGroupCount.width, texture.height / threadGroupCount.height, 1)
@@ -100,8 +98,48 @@ class ViewController: UIViewController
         commandQueue = device.newCommandQueue()
         
         commandEncoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadGroupCount)
+        commandEncoder.endEncoding()
+        
+        // write image....
+        
+        let imageSize = CGSize(width: outTexture.width, height: outTexture.height)
+        let imageByteCount = imageSize.width * imageSize.height * 4
+        let bytesPerPixel = UInt(8)
+        let bitsPerComponent = UInt(8)
+        let bitsPerPixel:UInt = 32
+        let bytesPerRow = bytesPerPixel * UInt(imageSize.width)
+        var imageBytes = [UInt](count: Int(imageSize.width * imageSize.height * 4), repeatedValue: 0)
+        let region = MTLRegionMake2D(0, 0, Int(imageSize.width), Int(imageSize.height))
+        
+        outTexture.getBytes(&imageBytes, bytesPerRow: Int(bytesPerRow), fromRegion: region, mipmapLevel: 0)
+        
+        let providerRef = CGDataProviderCreateWithCFData(NSData(bytes: &imageBytes, length: imageBytes.count))
+        
+        let cgim = CGImageCreate(
+            UInt(imageSize.width),
+            UInt(imageSize.height),
+            UInt(bitsPerComponent),
+            UInt(bitsPerPixel),
+            UInt(imageSize.width * 4),
+            rgbColorSpace,
+            bitmapInfo,
+            providerRef,
+            nil,
+            true,
+            kCGRenderingIntentDefault
+        )
+        
+        imageView.image = UIImage(CGImage: cgim)
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: imageSize.width, height: imageSize.height)
     }
-     override func didReceiveMemoryWarning()
+    
+    override func viewDidLayoutSubviews()
+    {
+        
+    }
+    
+    override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
